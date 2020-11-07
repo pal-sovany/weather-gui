84e49da71f9d23283a78bce8e0559e2396a0f05f
Pal Sovany <sovany.pal@gmail.com>
2020-11-07 10:41:45 +0100

    Minor fix

--- src/main/java/com/palsovany/App.java
+++ src/main/java/com/palsovany/App.java
@@ -17,22 +17,12 @@ public final class App {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello World Swing Example");
         JLabel lblText = new JLabel("Hello World!");
+        lblText.setBounds(130,100,100, 40);
 
         frame.add(lblText);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 500);
         frame.setLayout(null);
         frame.setVisible(true);
-
-        // JFrame f=new JFrame();//creating instance of JFrame  
-          
-        // JButton b=new JButton("click");//creating instance of JButton  
-        // b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-                
-        // f.add(b);//adding button in JFrame  
-                
-        // f.setSize(400,500);//400 width and 500 height  
-        // f.setLayout(null);//using no layout managers  
-        // f.setVisible(true);//making the frame visible  
     }
 }
